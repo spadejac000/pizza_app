4292eee4c8795e0373484347742813829cc491fc
Jacob Spade <spade.jacob@Yahoo.com>
2019-06-14 12:50:27 -0700

    created logic for food types with images

--- client/src/components/pages/RestFoodTypes.js
+++ client/src/components/pages/RestFoodTypes.js
@@ -11,37 +11,27 @@ class RestFoodTypes extends Component {
     return (
       <Consumer>
         {value => {
-          // where console log starts
-          value.restaurants.map((restaurant) => (
-            restaurant.path === this.props.match.path 
-            ? Object.keys(restaurant).slice(3).map((foodTypes) => {
-              for(let i = 0; i < Object.keys(value.foodImagesMain).length; i++) {
-                if(Object.keys(value.foodImagesMain)[i] === foodTypes) {
-                  console.log(foodTypes)
-                }
-              }
-              })
-            : null
-          ))
-          // where console log ends
           return(
             value.restaurants.map((restaurant) => (
-              <Container>
-                {
-                  restaurant.path === this.props.match.path ?
-                  Object.keys(restaurant).slice(3).map((foodTypes) => (
-                    <h1>{foodTypes}</h1>
-                  // <Link className="foodTypeLinks mb-2">
-                  //   <img src={foodTypes.image} />
-                  //   {foodTypes}
-                  // </Link>
-                  ))
-                : 
-                  null
+              <Container>{
+              restaurant.path === this.props.match.path 
+              ? Object.keys(restaurant).slice(3).map((foodTypes) => {
+                for(let i = 0; i < Object.keys(value.foodImagesMain).length; i++) {
+                  if(Object.keys(value.foodImagesMain)[i] === foodTypes) {
+                    return (
+                      <Link className="foodTypeLinks mb-2">
+                        <img src={value.foodImagesMain[foodTypes]} />
+                        {foodTypes}
+                      </Link>
+                    )
+                  }
                 }
-              </Container>
+                })
+              : null
+              }</Container>
             ))
           )
+
         }}
       </Consumer>
     )
