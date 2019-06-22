4e14b6dda31b9e680dff438fc06d44ede650bd1b
Jacob Spade <spade.jacob@Yahoo.com>
2019-06-22 14:50:45 -0700

    now able to post to database

--- routes/api/pizzas.js
+++ routes/api/pizzas.js
@@ -6,7 +6,32 @@ const Pizza = require('../../models/Pizza')
 
 // GET api/pizzas
 router.get('/', (req, res) => {
-  console.log(nearbyStores, 'Sam I am');
+  Pizza.find()
+    .then(pizzas => res.json(pizzas))
+})
+
+// POST api/pizzas
+router.post('/', (req, res) => {
+  const newPizza = new Pizza({
+    pizzaName: req.body.pizzaName,
+    price: req.body.price,
+    quantity: req.body.quantity,
+    image: req.body.image
+  });
+
+  newPizza.save().then(pizza => res.json(pizza))
+})
+
+// DELETE api/pizzas
+router.delete('/', (req, res) => {
+  const newPizza = new Pizza({
+    pizzaName: req.body.pizzaName,
+    price: req.body.price,
+    quantity: req.body.quantity,
+    image: req.body.image
+  });
+
+  newPizza.save().then(pizza => res.json(pizza))
 })
 
 module.exports = router;
