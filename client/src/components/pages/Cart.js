2e1b206e346743eec30d07e2d880495b2954083b
Jacob Spade <spade.jacob@Yahoo.com>
2019-06-24 23:06:42 -0700

    styling cart list

--- client/src/components/pages/Cart.js
+++ client/src/components/pages/Cart.js
@@ -1,25 +1,33 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import {Container, ListGroup, ListGroupItem} from 'react-bootstrap';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 class Cart extends Component {
   state = {
-    cart: []
+    food: []
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/api/pizzas').then(res => {
-      const cart = res.data;
-      this.setState({ cart });
+      const food = res.data;
+      this.setState({ food });
     })
   }
 
   render() {
     return(
-      <ul>
-        { this.state.cart.map(cart => <li>{cart.pizzaName}</li>)}
-      </ul>
+      <Container>
+        <ListGroup>
+          { this.state.food.map(food => <ListGroup.Item><img src={food.image} />{food.pizzaName} {food.quantity} {food.price}</ListGroup.Item>)}
+        </ListGroup>
+      </Container>
     )
   }
 }
+
+// const cartItemsStyle = {
+//   border:
+// }
 
 export default Cart;
