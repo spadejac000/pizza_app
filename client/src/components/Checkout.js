3ae3725588b7565d1555d19c4cf9c2dc7f5761da
Jacob Spade <spade.jacob@Yahoo.com>
2019-07-04 00:50:33 -0700

    can now open stripe creditcard form with button

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,25 +1,28 @@
 import React, {Component} from 'react';
 
 class Checkout extends Component {
+
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "https://checkout.stripe.com/checkout.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return(
       <form action="/charge" method="POST">
         <script
-          src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+          src="https://checkout.stripe.com/checkout.js" className="stripe-button"
           // data-key={{stripePublishableKey}}
           data-amount="2500"
           data-name="Pizza App Checkout"
-          data-description="Ebook written by Jacob Spade"
-          data-image="/img/pepsi.png"
+          data-description="Pizza app cart. Checking out items"
+          data-image="https://cdn.dribbble.com/users/404971/screenshots/1241486/pizza-logo.png"
           data-locale="auto">
         </script>
-        <script>
-          // hide default stripe button
-          document.getElementsByClassName('stripe-button-el')[0].style.display = 'block';
-        </script>
-        <button type="submit" class="btn btn-outline-dark text-white btn">
-          Purchase For $25
-        </button>
       </form>
     )
   }
